e6f3cd85e6c23a50590a0b23ca5b9c01752a8491
David-Peede <jakepeede@gmail.com>
2021-05-19 22:07:11 -0400

    Update vcf_DP_qc.py

--- data_processing/vcf_DP_qc.py
+++ data_processing/vcf_DP_qc.py
@@ -2,12 +2,17 @@ import allel
 import numpy as np
 import sys
 
-VCF = str(sys.argv[1])
-callset = allel.read_vcf(VCF, fields=['DP'])
+# sys.argv[1] = sample prefix
+# sys.argv[2] = reference genome
+# sys.argv[3] = chromosome
+
+unfiltered_vcf = str(sys.argv[1])+'_unfiltered_m'+str(sys.argv[2])+'_chr'+str(sys.argv[3])+'.vcf.gz'
+callset = allel.read_vcf(unfiltered_vcf, fields=['DP'])
 
 DP = callset['variants/DP']
 
 lower_bound = np.percentile(DP, 2.5)
 upper_bound = np.percentile(DP, 97.5)
 
-print("bcftools view -i 'DP>="+lower_bound+" && DP<="+upper_bound+" && MQ>=30' -Oz -o "+VCF)
+filtered_vcf = str(sys.argv[1])+'_filtered_DP_m'+str(sys.argv[2])+'_chr'+str(sys.argv[3])+'.vcf.gz'
+print("bcftools view -i 'DP>="+lower_bound+" && DP<="+upper_bound+" && QUAL>=20 && MQ>=30' -Oz -o "+filtered_vcf+" "+unfiltered_vcf)
