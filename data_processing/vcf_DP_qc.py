2d4ef277972cdff33edea7c58a1665394e63a26a
David-Peede <jakepeede@gmail.com>
2021-05-19 23:34:47 -0400

    Update vcf_DP_qc.py

--- data_processing/vcf_DP_qc.py
+++ data_processing/vcf_DP_qc.py
@@ -15,4 +15,4 @@ lower_bound = np.percentile(DP, 2.5)
 upper_bound = np.percentile(DP, 97.5)
 
 filtered_vcf = str(sys.argv[1])+'_filtered_DP_m'+str(sys.argv[2])+'_chr'+str(sys.argv[3])+'.vcf.gz'
-print("bcftools view -i 'DP>="+lower_bound+" && DP<="+upper_bound+" && QUAL>=20 && MQ>=30' -Oz -o "+filtered_vcf+" "+unfiltered_vcf)
+print("bcftools view -i 'DP>="+str(lower_bound.astype(int))+" && DP<="+str(upper_bound.astype(int))+" && QUAL>=20 && MQ>=30' -Oz -o "+filtered_vcf+" "+unfiltered_vcf)
